9529286c3bf09994f770b80003d631be44a0c185
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-17 17:21:01 +0700

    add

---
+++ src/DataType.java
@@ -0,0 +1,36 @@
+public class DataType {
+    public static void main(String[] args) {
+
+        // so nguyen;
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000l;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+
+        // So thuc;
+        float f = 30.400f;
+        double d = 4000.44;
+
+        System.out.println(f);
+        System.out.println(d);
+
+        // Kieu dung/sai;
+        boolean t = true;
+        boolean si = false;
+
+        System.out.println(t);
+        System.out.println(si);
+
+        // Kieu ky tu;
+        String x = "Thao";
+        String y = "Hoa";
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
